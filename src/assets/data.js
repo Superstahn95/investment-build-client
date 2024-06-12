07a4847dbed86eb7f05216ce18b681f1b3d319b6
superstahn <superstahn@gmail.com>
2024-06-12 15:36:25 +0100

    made some changes to the data and the some parts of the UI as well

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,10 +1,10 @@
 export const paymentMethods = [
   {
-    name: "usdt",
+    name: "usdt bep20",
     logo: "https://cryptologos.cc/logos/tether-usdt-logo.png?v=024",
     speed: "Fast",
     gasFee: "low gas fee",
-    walletAddress: "thisismyusdtwalletaddress",
+    walletAddress: "0x623546882c086272ac57da424f8a9137e7c4c62c",
     network: "bep20",
   },
   {
@@ -12,7 +12,7 @@ export const paymentMethods = [
     logo: "https://cryptologos.cc/logos/bitcoin-btc-logo.png?v=026",
     speed: "Averagely fast",
     gasFee: "slightly higher gas fee",
-    walletAddress: "thisismybitcoinwalletaddress",
+    walletAddress: "1AgR4py8JKa9W5WZtsvR21xBBowRHsUFTa",
     network: "bitcoin",
   },
   {
@@ -20,7 +20,7 @@ export const paymentMethods = [
     logo: "https://cryptologos.cc/logos/ethereum-eth-logo.png?v=026",
     speed: "Averagely fast",
     gasFee: "High gas fee",
-    walletAddress: "thisismyethereumwalletaddress",
+    walletAddress: "0x623546882c086272ac57da424f8a9137e7c4c62c",
     network: "erc20",
   },
   {
@@ -28,7 +28,7 @@ export const paymentMethods = [
     logo: "https://cryptologos.cc/logos/tron-trx-logo.png?v=026",
     speed: "Fast",
     gasFee: "low gas fee",
-    walletAddress: "thisismytronwalletaddress",
+    walletAddress: "TM1XV5Cm6t7FHiSVvyrq3DX9kdcqHCBqCC",
     network: "trc20",
   },
 ];
